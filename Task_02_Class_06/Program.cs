0c47cb89f16ad5d7a4851ac88b7b340decff4001
Majjja <45804941+Majjja@users.noreply.github.com>
2020-04-06 14:58:36 +0200

    test

---
+++ Task_02_Class_06/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_02_Class_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
