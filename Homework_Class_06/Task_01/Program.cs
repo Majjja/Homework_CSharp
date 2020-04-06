08a794e42f5d3d97487e0f73cfc5c71c421a4c8f
Majjja <45804941+Majjja@users.noreply.github.com>
2020-04-06 16:57:02 +0200

    homework_class_06

---
+++ Homework_Class_06/Task_01/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Task_01
+{
+    public class PhotoAlbum
+    {
+        public PhotoAlbum()
+        {
+            NumberOfPages = 16;
+        }
+
+        public PhotoAlbum(int numOfPages)
+        {
+            NumberOfPages = numOfPages;
+        }
+
+        private int NumberOfPages;
+
+        public int GetNumberOfPages()
+        {
+            return NumberOfPages;
+        }
+    }
+
+    public class BigPhotoAlbum
+    {
+        public BigPhotoAlbum()
+        {
+            NumberOfPages = 64;
+        }
+
+        private int NumberOfPages;
+        public int GetNumberOfPages()
+        {
+            return NumberOfPages;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PhotoAlbum defaultNumOfPages = new PhotoAlbum();
+            var desiredNumOfPages = new PhotoAlbum(24);
+
+            var bigPhotoAlbumPages = new BigPhotoAlbum();
+
+            Console.WriteLine($"Number of pages that the three albums have are:" +
+                $"\n{defaultNumOfPages.GetNumberOfPages()}\n{desiredNumOfPages.GetNumberOfPages()}" +
+                $"\n{bigPhotoAlbumPages.GetNumberOfPages()}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework_Class_06/Task_02/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Task_02
+{
+    public class FirstCourse
+    {
+        public string CourseName;
+        public bool PassedTheCourse;
+
+        public bool Passed()
+        {
+            if (PassedTheCourse) return true;
+            else return false;
+        }
+    }
+
+    public class SecondCourse
+    {
+        public string CourseName;
+        public StudentsGrades Grade;
+
+        public bool Passed()
+        {
+            if ((int)Grade > 5) return true;
+            else return false;
+        }
+    }
+
+    public class Project
+    {
+        public Project(bool course1, bool course2, bool course3, bool course4)
+        {
+            Course1 = course1;
+            Course2 = course2;
+            Course3 = course3;
+            Course4 = course4;
+        }
+
+        public bool Course1;
+        public bool Course2;
+        public bool Course3;
+        public bool Course4;
+
+        public bool[] Courses;
+
+        public void Passed()
+        {
+            Courses = new bool[] { Course1, Course2, Course3, Course4 };
+            int count = 0;
+            foreach (var course in Courses)
+            {
+                if (course)
+                    count++;
+            }
+            if (count >= 3)
+                Console.WriteLine("The student has Passed!");
+            else
+                Console.WriteLine("The student didn't pass.");
+        }
+    }
+
+    public enum StudentsGrades
+    {
+        Five = 5,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var course1 = new FirstCourse()
+            {
+                CourseName = "Java Script Basic",
+                PassedTheCourse = true
+            };
+
+            var course2 = new FirstCourse()
+            {
+                CourseName = "Java Script Advanced",
+                PassedTheCourse = true
+            };
+
+            var course3 = new SecondCourse()
+            {
+                CourseName = "C# Basic",
+                Grade = StudentsGrades.Nine
+            };
+
+            var course4 = new SecondCourse()
+            {
+                CourseName = "C# Advanced",
+                Grade = StudentsGrades.Eight
+            };
+
+            var projectPass = new Project(
+                course1.Passed(),
+                course2.Passed(),
+                course3.Passed(),
+                course4.Passed()
+                );
+
+            projectPass.Passed();
+
+            Console.ReadLine();
+        }
+    }
+}
