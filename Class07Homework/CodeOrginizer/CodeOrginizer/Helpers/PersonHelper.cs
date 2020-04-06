a8104f1e4763d40ca051d6ce3701765f31fed96a
Majjja <45804941+Majjja@users.noreply.github.com>
2020-04-06 22:53:23 +0200

    Homework-class7-part1

---
+++ Class07Homework/CodeOrginizer/CodeOrginizer/Helpers/PersonHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeOrginizer.Helpers
+{
+    public class PersonHelper
+    {
+        public static long GenerateSSN()
+        {
+            Random rand = new Random();
+            return rand.Next(1000000, 9999999);
+        }
+    }
+}
